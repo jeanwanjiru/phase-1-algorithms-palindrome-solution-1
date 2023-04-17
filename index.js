cd070a35ae4300262b156295549c24cc025db24c
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-04-17 11:05:31 -0400

    done algorithm solution 1

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
 if (require.main === module) {
-  // add your own custom tests in here
+ 
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
